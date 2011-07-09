0050ad9a0cc6584be83ff5c071531f5feae68cee
Raul Riera <rieraraul@gmail.com>
2011-07-09 21:48:14 +0200

    Oops bug fixed

--- rater.js
+++ rater.js
@@ -10,7 +10,7 @@ var Rater = {
 	appName:'',
 	appId:'',
 	appLaunches: 20,
-	appUsageInSeconds: 0 // 0 if you don't want to use this
+	appUsageInSeconds: 30 // 0 if you don't want to use this
 };
 
 Rater.data = {
@@ -22,6 +22,9 @@ Rater.data = {
 Rater.startUsageTimer = function(){
 	Rater.usageTimerInterval = setInterval(function() {
 	    Rater.data.timeUsed++;
+	    
+	    // Debugging
+	    // Ti.API.info(Rater.data.timeUsed + "seconds of app usage");
 	    
 	    // Check if the desired usage time has been reached
 	    if(Rater.data.timeUsed === Rater.appUsageInSeconds) {
@@ -46,12 +49,10 @@ Rater.initUsageCounter = function() {
 };
 
 Rater.load = function() {
-	var prop = Ti.App.Properties.getString("RaterData", null);
-	if(prop) {
-		Rater.data = JSON.parse(prop);
-	}
+	// Read the data
+	Rater.read();
 	
-	// Increase the launche count
+	// Increase the launch count
 	Rater.data.launchCount++;
 	
 	// Init the usage counter
@@ -60,6 +61,13 @@ Rater.load = function() {
 	// Save the data
 	Rater.save();
 };
+
+Rater.read = function() {
+	var prop = Ti.App.Properties.getString("RaterData", null);
+	if(prop) {
+		Rater.data = JSON.parse(prop);
+	}
+}
 
 Rater.save = function() {
 	Ti.App.Properties.setString("RaterData", JSON.stringify(Rater.data));
@@ -75,8 +83,8 @@ Rater.openRateDialog = function() {
 	var a = Ti.UI.createAlertDialog({
 		title: L("rating_title"),
 		message: L("rating_message"),
-		buttonNames:[L("rating_option_1"), L("rating_option_2"), L("rating_option_3")],
-		cancel:2
+		buttonNames: [L("rating_option_1"), L("rating_option_2"), L("rating_option_3")],
+		cancel: 2
 	});
 	a.addEventListener('click',function(e){
 		switch(e.index) {
@@ -94,14 +102,23 @@ Rater.openRateDialog = function() {
 	a.show();
 }
 
-Rater.init = function(_appName, _appId, _appLaunches, _appUsageInSeconds) {
+Rater.init = function(_appName, _appId) {
 	Rater.load();
 	
 	// Set the default values
 	Rater.appName = _appName;
 	Rater.appId = _appId;
-	Rater.appLaunches = _appLaunches;
-	Rater.appUsageInSeconds = _appUsageInSeconds;
 	
 	Rater.run();
 };
+
+
+Ti.App.addEventListener('resumed',function(e){	
+	// Read the data again
+	Rater.read();
+});
+
+Ti.App.addEventListener('pause',function(e){	
+	// Save the data
+	Rater.save();
+});
